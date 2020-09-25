fd58504ba5dc70d803835f84164dbe95b8c44c92
Nayra <63517637+xNayra@users.noreply.github.com>
2020-09-25 16:02:22 +0200

    Add files via upload

---
+++ antivpnIDs.js
@@ -0,0 +1,14 @@
+// How to use - ¿Cómo usar?
+// IPASG = VPN, ejemplo 198.0.x.x
+// Al banearlas no garantiza un AntiVPN total, pero si la prohibición 5-20% de los siguientes programas.
+
+// Windscribe 
+// Hotspot Shield 
+// Betternet
+
+var avpn = [
+	"743785413855936562",
+	"743784877702381660",
+	"743784435018891374",
+	"743785801313288313"
+];
