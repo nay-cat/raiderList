a0625522d75a44cadc6ea2d662b5ab9732a2ba5e
Nayra <63517637+xNayra@users.noreply.github.com>
2020-09-25 16:02:59 +0200

    Update antivpnIDs.js

--- antivpnIDs.js
+++ antivpnIDs.js
@@ -1,6 +1,6 @@
 // How to use - ¿Cómo usar?
 // IPASG = VPN, ejemplo 198.0.x.x
-// Al banearlas no garantiza un AntiVPN total, pero si la prohibición 5-20% de los siguientes programas.
+// Al banearlas no garantiza un AntiVPN total, pero si la prohibición 5-20% de los siguientes programas:
 
 // Windscribe 
 // Hotspot Shield 
